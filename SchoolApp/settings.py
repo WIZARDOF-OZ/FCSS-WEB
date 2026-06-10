baca2f2e248645af9fa986aa73f41f42c119fc9a
mrinmoy-hex <init2350@gmail.com>
2026-06-10 16:11:07 +0530

    fix: remove unused SCHOOL_EMAIL variable and add EMAIL_TIMEOUT setting

--- SchoolApp/settings.py
+++ SchoolApp/settings.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import os
 import environ
 from django.templatetags.static import static
-SCHOOL_EMAIL = os.environ.get('SCHOOL_EMAIL')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(DEBUG=(bool, True))
@@ -125,6 +124,8 @@ CACHES = {
     }
 }
 
+
+EMAIL_TIMEOUT = 10
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
@@ -132,6 +133,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+
 
 # Unfold Customization
 
